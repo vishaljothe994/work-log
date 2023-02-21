ae65c56fa76ba2506f4736ce718c5660048729ed
vishaljothe994 <jothevishal994@gmail.com>
2023-02-21 18:00:01 +0530

    class base APIview & Generic View

---
+++ class_based_generic_view/project1/project1/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from app1 import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('studentapi/', views.LCStudentAPI.as_view()),
+    path('studentapi/<int:pk>/', views.RUDStudentAPI.as_view()),
+]
